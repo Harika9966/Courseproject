ade682cf32d11569bd8309f9441aa30f416d84b1
Harika Mamindla <harikamamindla12@gmail.com>
2022-03-03 13:31:38 +0530

    observables

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
 
@@ -7,17 +8,23 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit,OnDestroy {
   ingredients!: Ingredient[];
   // ingredients: Ingredient[] = [
   //   new Ingredient('Apples',5),
   //   new Ingredient('Mangos',10),
   // ];
   constructor(private slService:ShoppingListService) { }
-
+  private igChangeSub!:Subscription;
   ngOnInit() {
     this.ingredients=this.slService.getIngredient();
-    this.slService.ingredientsChanged
+    // this.slService.ingredientsChanged
+    // .subscribe(
+    //   (ingredients:Ingredient[]) => {
+    //     this.ingredients = ingredients;
+    //   }
+    // )
+     this.igChangeSub = this.slService.ingredientsChanged
     .subscribe(
       (ingredients:Ingredient[]) => {
         this.ingredients = ingredients;
@@ -27,5 +34,8 @@ export class ShoppingListComponent implements OnInit {
   // onIngredientAdded(ingredient:Ingredient) {
   //  // this.ingredients.push(ingredient);
   // }
+  ngOnDestroy(): void {
+      this.igChangeSub.unsubscribe();
+  }
 
 }
